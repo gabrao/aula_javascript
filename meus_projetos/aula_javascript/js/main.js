92bee889e58369e4a8bd2532f4a4828ca4a66b63
gabrao <71773990+gabrao@users.noreply.github.com>
2021-01-21 13:10:03 -0300

    Add files via upload

---
+++ meus_projetos/aula_javascript/js/main.js
@@ -0,0 +1,102 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<i>Obrigado por clicar!</i>"
+}
+
+function clicou1(){
+    window.open("https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d");
+    window.location.href = "https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passa não véi";
+    elemento.innerHTML = "Passa não véi"
+    console.log("Vc passou o mouse por cima to txto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("page loaded!");
+}
+
+function functionChange(elemento){
+    console.log(elemento.value);
+}
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+//alert(soma(10, 15));
+console.log(setReplace("vai japão", "japão", "brasil"));
+*/
+
+/*var d = new Date();
+alert(d.getMonth()+1);
+*/
+
+/*var count;
+for(count = 0; count <=10; count++){
+    console.log(count);
+};
+*/
+
+/*
+var count = 0;
+while (count < 100){
+    console.log(count);
+    count++;
+}
+*/
+
+
+
+
+
+
+
+/*var idade2 = prompt("qual a sua idade:");
+var idade = 18;
+
+if (idade2 >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+
+
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"Roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+*/
+
+//var nome = "Gabriel Arão";
+//var idade = "23";
+//var idade2 = 10;
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva")/
+//lista.pop();
+//console.log(lista.replace("maçã", "melão"))
+//console.log(lista.length)
+//console.log(lista.reverse)
+//console.log(lista.toString()[0])
+//console.log(lista.join(" | "))
+
+
+
+//alert(nome + " tem " + idade + " anos");
